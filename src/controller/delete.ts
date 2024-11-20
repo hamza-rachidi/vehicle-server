629cf4bcef78a3b7f050f9b41a762b36cb27e10a
lin0qlin Lin <qiulinglin00@gmail.com>
2024-11-20 10:28:12 +0000

    Modifier DeleteVehicleController V2

--- src/controller/delete.ts
+++ src/controller/delete.ts
@@ -2,27 +2,20 @@ import { VehicleStore } from '../store/vehicle';
 import { Request, Response } from 'express';
 
 interface Parameters {
-  id: string;
+ id: string;
 }
 
 export class DeleteVehicleController {
-  constructor(private readonly vehicleStore: VehicleStore) {}
+ constructor(private readonly vehicleStore: VehicleStore) {}
 
-  public async handle(req: Request<Parameters>, res: Response): Promise<void> {
-    try {
-      const id = parseInt(req.params.id, 10);
-      
-      if (isNaN(id) || id < 1) {
-        res.status(400).send({ error: 'Invalid ID' });
-        return;
-      }
-
-      await this.vehicleStore.deleteVehicle(id);
-
-      res.status(204).send();
-    } catch (error) {
-      console.error('Error deleting vehicle:', error);
-      res.status(500).send({ error: 'Internal Server Error' });
-    }
-  }
+ public async handle(req: Request<Parameters>, res: Response): Promise<void> {
+   const idParam = req.params.id; // Extraire l'identifiant du path
+   // Parser l'identifiant en nombre
+   const id = parseInt(idParam, 10);
+   
+   const isDeleted = await this.vehicleStore.deleteVehicle({id : id});
+     
+   res.status(204).send();
+     
+ }
 }
